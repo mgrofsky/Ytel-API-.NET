e92515d13b8d53aba0684b01052e517e036106c4
mgrofsky <git@apimatic.io>
2018-10-11 03:17:36 +0000

    Modified the base url to api.ytel.com

---
+++ YtelAPIV3.Standard/Utilities/CustomDateTimeConverter.cs
@@ -0,0 +1,12 @@
+using Newtonsoft.Json.Converters;
+
+namespace YtelAPIV3.Standard.Utilities
+{
+    public class CustomDateTimeConverter : IsoDateTimeConverter
+    {
+        public CustomDateTimeConverter(string format)
+        {
+            DateTimeFormat = format;
+        }
+    }
+}
